fabab6bdc72dfbc541f8dc6676a5ab7da12bbacc
Richard Temple <richardtemple@mutexmusic.com>
2015-12-18 01:55:14 +0000

    fixing AWS

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -13,7 +13,7 @@ class PictureUploader < CarrierWave::Uploader::Base
   else
     storage :file
   end
-  # storage :fog
+  
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
